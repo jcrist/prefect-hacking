52cde50c73b28ad27e6d0c5610fa19989caaf0b0
Jim Crist-Harif <jcristharif@gmail.com>
2021-02-11 14:14:04 -0600

    Add run config

--- test_github.py
+++ test_github.py
@@ -1,14 +1,19 @@
+import os
+
 from prefect import Flow, task
 from prefect.storage import GitHub
+from prefect.run_configs import LocalRun
 
 
 @task(log_stdout=True)
-def echo(x):
-    print(x)
+def greet(name):
+    greeting = os.environ.get("GREETING", "Hello")
+    print(f"{greeting}, {name}!")
 
 
 with Flow("test-github") as flow:
-    echo("hello")
+    greet("hello")
 
 
 flow.storage = GitHub("jcrist/prefect-hacking", path="test_github.py")
+flow.run_config = LocalRun(env={"GREETING": "Hello"})
